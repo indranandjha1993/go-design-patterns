a1bab64ae8d5b872e58f7a1f1622c1564f496aa0
Indra Nand Jha <indranandjha1993@gmail.com>
2022-04-22 11:46:09 +0530

    Setup

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/fatih/color"
+)
+
+var d = color.New(color.FgHiYellow, color.Bold)
+
+func main() {
+	d.Println("############################################################ Creational Design Pattern ############################################################")
+
+}
